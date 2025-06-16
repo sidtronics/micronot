eaf19f6b935dfc8cb65ff06b2363e202e0162f3b
Siddhesh Dharme <siddheshdharme18@gmail.com>
2025-06-16 13:44:35 +0530

    style: formatting changes

--- src/list.h
+++ src/list.h
@@ -11,10 +11,10 @@ struct _NotificationNode {
   NotificationNode *next;
 };
 
-Notification* notification_list_append(NotificationNode **head);
+Notification *notification_list_append(NotificationNode **head);
+
+NotificationNode *notification_list_unlist_next(NotificationNode *current);
 
 void notification_list_remove_next(NotificationNode *current);
-
-NotificationNode* notification_list_unlist_next(NotificationNode *current);
 
 #endif
