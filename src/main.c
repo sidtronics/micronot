7d672de414bd680a666d486c7539fc4cabb0e370
Siddhesh Dharme <siddheshdharme18@gmail.com>
2025-06-22 02:05:28 +0530

    wip: modified the entry point to test new architecture

--- src/main.c
+++ src/main.c
@@ -1,74 +1,84 @@
-#include <X11/Xft/Xft.h>
-#include <X11/Xlib.h>
-#include <assert.h>
-#include <time.h>
-#include <unistd.h>
+#include <locale.h>
+#include "unotd.h"
 
-#include "config.h"
-#include "icon.h"
-#include "notification.h"
+Unotd unotd = {
 
-Bool handle_events(Display *display) {
-  XEvent e;
-  while (XPending(display)) {
-    XNextEvent(display, &e);
-    switch (e.type) {
+    .origin = UNOT_ORIGIN_BOTTOM_LEFT,
+    .head_open = NULL,
+    .head_waiting = NULL,
 
-    case Expose:
-      break;
-    case ButtonPress:
-      if (e.xbutton.button == Button1) {
-        XUnmapWindow(display, e.xbutton.window);
-        return True;
-      }
-      break;
-    }
-  }
-  return False;
+    .config =
+        {
+            .x_padding = 2,
+            .y_padding = 2,
+            .spacing = 5,
+            .border_thickness = 2,
+            .gap_size = 7,
+            .indicator_size = 12.0,
+            .background_color = 0x000000,
+            .foreground_color = 0x00FF00,
+            .border_color = 0x00FF00,
+        },
+
+};
+
+static void allocate_color(Display *dpy, unsigned long color, XftColor *res) {
+
+  XRenderColor xrcolor = {.red = ((color >> 16) & 0xff) * 257,
+                          .green = ((color >> 8) & 0xff) * 257,
+                          .blue = (color & 0xff) * 257,
+                          .alpha = 0xffff};
+
+  int scr_nbr = DefaultScreen(dpy);
+
+  XftColorAllocValue(dpy, DefaultVisual(dpy, scr_nbr),
+                     DefaultColormap(dpy, scr_nbr), &xrcolor, res);
 }
 
-int main(int argc, char **argv) {
+static void append_new_not(Unotd *unotd, NotificationType t, const char *m,
+                           const char *i, int to) {
 
-  assert(argc == 3 && "argument error");
-  const char *text = argv[1];
-  const char *ico = argv[2];
-  //  assert(strlen(icon) == 6 * 3 && "icon error");
+  Notification *new_not = notification_list_append(&unotd->head_open, NULL);
+  new_not->window = 0;
+  new_not->frame = NULL;
+  new_not->msg_font = NULL;
+  new_not->ind_color = NULL;
+  new_not->msg_color = NULL;
+  new_not->timeout = to;
+  new_not->type = t;
+  new_not->message = strdup(m);
+  new_not->indicator = i;
+  new_not->ind_font = unotd_resolve_indicator_font(unotd, new_not->indicator);
+  assert(new_not->ind_font && "font not resolved");
+}
 
-  Display *display = XOpenDisplay(NULL);
-  assert(display);
+int main() {
 
-  u_int32_t scr_nbr = DefaultScreen(display);
+  setlocale(LC_ALL, "en_US.utf8");
+  unotd.display = XOpenDisplay(NULL);
+  unotd.msg_font = XftFontOpenName(unotd.display, DefaultScreen(unotd.display),
+                                   "FiraCodeNerdFontPropo:style=Bold:size=8");
 
-  XftFont *font = XftFontOpenName(display, scr_nbr,
-                                  "FiraCodeNerdFontMono:style=Bold:size=8");
-  XftFont *ifont = XftFontOpenName(display, scr_nbr,
-                                   "FiraCodeNerdFontMono:style=Bold:size=12");
+  allocate_color(unotd.display, unotd.config.foreground_color,
+                 &unotd.ind_color);
+  allocate_color(unotd.display, unotd.config.foreground_color,
+                 &unotd.msg_color);
 
-  assert(font && "font error");
+  append_new_not(&unotd, UNOT_SPINNER, "Updating system",
+                 "\x1E\x1E\x1E\x1E\x1E\0\0\0", 20);
 
-  Config config = (Config){5, 5, 5, 2, 0x000000, 0x00ff00, 0x00ff00};
+  append_new_not(&unotd, UNOT_MESSAGE, "Reminder Drink Water!", "💧\0", 10);
 
-  Spinner *spinner = CreateSpinner(
-      display, ifont,
-      (const char *[]){"", "", "", "", "", "", "", ""},
-      6);
+  append_new_not(&unotd, UNOT_SPINNER, "Syncing mirrors",
+                 "🌍\x1E🌎\x1E🌏\0✔️\0✖️\0", 20);
 
-  assert(spinner);
-
-  Icon *icon = CreateIcon(display, ifont, ico);
-
-  Notification *not = notification_open(display, &config, font, text,
-                                               UNOT_SPINNER, spinner, 5);
+  append_new_not(&unotd, UNOT_MESSAGE, "Now playing: FATRAT", "🎶\0", 30);
 
   while (1) {
-    if (handle_events(display))
-      break;
+    unotd_handle_events(&unotd);
+    unotd_update_notifications(&unotd);
 
-    notification_update(display, not);
-
-    struct timespec ts = {0, 10 * 1000000}; // 10ms
+    struct timespec ts = {0, 10 * 1000 * 1000};
     nanosleep(&ts, NULL);
   }
-
-  XCloseDisplay(display);
 }
