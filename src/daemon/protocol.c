43e60fad49d2a2db3b1642c97b741848236941b8
Siddhesh Dharme <siddheshdharme18@gmail.com>
2025-07-10 16:22:39 +0530

    feat: simplified protocol and improved logging
    + introduced another field "typ" which determines the notification type.
    + combined command "MSG" and "SPN" into single command "NTF".

--- src/daemon/protocol.c
+++ src/daemon/protocol.c
@@ -11,7 +11,7 @@ int protocol_recv_command(int fd, char *buf, size_t len) {
 
     if (bytes_read >= 255) {
 
-      fprintf(stderr, "error: buffer overflow\n");
+      fprintf(stderr, "[unotd:server] ERROR: command too large\n");
       send(fd, "ERROR\n\n", 7, 0);
       return 1;
     }
@@ -21,10 +21,10 @@ int protocol_recv_command(int fd, char *buf, size_t len) {
     if (n <= 0) {
 
       if (n < 0) {
-        perror("recv");
+        perror("[unotd:server] ERROR: recv");
       }
 
-      fprintf(stderr, "info: client closed the connection\n");
+      fprintf(stderr, "[unotd:server] INFO: connection closed by the client\n");
       return 1;
     }
 
@@ -38,70 +38,99 @@ int protocol_recv_command(int fd, char *buf, size_t len) {
   return 0;
 }
 
-static bool _parse_fields(Unotd *unotd, Notification *target) {
+static bool _parse_ntf_fields(Unotd *unotd, Notification *target) {
 
   const char *txt = NULL;
   const char *ind = NULL;
+  bool got_type = false;
 
-  while (1) {
+  char *pair = NULL;
+  while ((pair = strtok(NULL, "\n"))) {
 
-    const char *field = strtok(NULL, ":");
+    char *colon = strchr(pair, ':');
+    if (!colon) {
+      fprintf(stderr, "[unotd:server] ERROR: malformed command: missing ':'\n");
+      return false;
+    }
+    *colon = '\0';
 
-    if (strncmp(field, "\n", 1) == 0)
-      break;
-
-    const char *value = strtok(NULL, "\n");
-    if (value == NULL) {
-      fprintf(stderr, "error: malformed field\n");
-      return 1;
+    char *key = pair;
+    char *val = colon + 1;
+    if (!key || !val) {
+      fprintf(stderr, "[unotd:server] ERROR: malformed key-value pair\n");
+      return false;
     }
 
-    if (strncmp(field, "txt", 3) == 0)
-      txt = value;
+    if (strncmp(key, "txt", 3) == 0)
+      txt = val;
 
-    else if (strncmp(field, "ind", 3) == 0)
-      ind = value;
+    else if (strncmp(key, "typ", 3) == 0) {
 
-    else if (strncmp(field, "nme", 3) == 0) {
+      if (strncmp(val, "msg", 3) == 0)
+        target->type = UNOT_TYPE_MESSAGE;
+      else if (strncmp(val, "spn", 3) == 0)
+        target->type = UNOT_TYPE_SPINNER;
+      else {
+        fprintf(stderr,
+                "[unotd:server] ERROR: unknown notification type: '%s'\n", val);
+        return false;
+      }
+
+      got_type = true;
+    }
+
+    else if (strncmp(key, "ind", 3) == 0)
+      ind = val;
+
+    else if (strncmp(key, "nme", 3) == 0) {
       for (size_t i = 0; i < unotd->config.indicators_count; i++) {
         char *indicator = unotd->config.indicators[i];
-        if (strncmp(value, indicator, strlen(value)) == 0) {
+        if (strncmp(val, indicator, strlen(val)) == 0) {
           target->indicator = indicator + strlen(indicator) + 1;
           break;
         }
       }
+      if (!target->indicator)
+        fprintf(stderr,
+                "[unotd:server] WARN: indicator of name '%s' not found\n", val);
     }
 
-    else if (strncmp(field, "tfn", 3) == 0)
-      target->txt_font = (void *)value;
+    else if (strncmp(key, "tfn", 3) == 0)
+      target->txt_font = (void *)val;
 
-    else if (strncmp(field, "tfg", 3) == 0)
-      target->txt_color = (void *)value;
+    else if (strncmp(key, "tfg", 3) == 0)
+      target->txt_color = (void *)val;
 
-    else if (strncmp(field, "ifg", 3) == 0)
-      target->ind_color = (void *)value;
+    else if (strncmp(key, "ifg", 3) == 0)
+      target->ind_color = (void *)val;
 
-    else if (strncmp(field, "tim", 3) == 0) {
-      if (!utils_parse_ul(value, &target->timeout, 10)) {
-        fprintf(stderr, "error: invalid timeout\n");
-        return 1;
+    else if (strncmp(key, "tim", 3) == 0) {
+      if (!utils_parse_ul(val, &target->timeout, 10)) {
+        fprintf(stderr, "[unotd:server] ERROR: invalid timeout value: '%s'\n",
+                val);
+        return false;
       }
     }
 
     else {
-      fprintf(stderr, "error: unknown field: \"%s\"\n", field);
-      return 1;
+      fprintf(stderr, "[unotd:server] ERROR: unknown key: '%s'\n", key);
+      return false;
     }
   }
 
-  if (txt == NULL) {
-    fprintf(stderr, "error: text not set\n");
-    return 1;
+  if (!txt) {
+    fprintf(stderr, "[unotd:server] ERROR: missing key: 'txt'\n");
+    return false;
   }
 
-  if (ind == NULL && target->indicator == NULL) {
-    fprintf(stderr, "error: indicator not set\n");
-    return 1;
+  if (!got_type) {
+    fprintf(stderr, "[unotd:server] ERROR: missing key: 'typ'\n");
+    return false;
+  }
+
+  if (!ind && !target->indicator) {
+    fprintf(stderr, "[unotd:server] ERROR: missing key: 'ind' or 'nme'\n");
+    return false;
   }
 
   if (target->indicator)
@@ -116,7 +145,7 @@ static bool _parse_fields(Unotd *unotd, Notification *target) {
     memcpy(target->indicator, ind, ind_len);
   }
 
-  return 0;
+  return true;
 }
 
 static bool _parse_ret_fields(unsigned long *wid, unsigned long *ret) {
@@ -124,52 +153,58 @@ static bool _parse_ret_fields(unsigned long *wid, unsigned long *ret) {
   bool got_wid = false;
   bool got_ret = false;
 
-  while (1) {
+  char *pair = NULL;
+  while ((pair = strtok(NULL, "\n"))) {
 
-    const char *field = strtok(NULL, ":");
+    char *colon = strchr(pair, ':');
+    if (!colon) {
+      fprintf(stderr, "[unotd:server] ERROR: malformed command: missing ':'\n");
+      return false;
+    }
+    *colon = '\0';
 
-    if (strncmp(field, "\n", 1) == 0)
-      break;
-
-    const char *value = strtok(NULL, "\n");
-    if (value == NULL) {
-      fprintf(stderr, "error: malformed field\n");
-      return 1;
+    char *key = pair;
+    char *val = colon + 1;
+    if (!key || !val) {
+      fprintf(stderr, "[unotd:server] ERROR: malformed key-value pair\n");
+      return false;
     }
 
-    if (strncmp(field, "wid", 3) == 0) {
-      if (!utils_parse_ul(value, wid, 10)) {
-        fprintf(stderr, "error: invalid window id\n");
-        return 1;
+    if (strncmp(key, "wid", 3) == 0) {
+      if (!utils_parse_ul(val, wid, 10)) {
+        fprintf(stderr, "[unotd:server] ERROR: invalid notification id: '%s'\n",
+                val);
+        return false;
       }
       got_wid = true;
     }
 
-    else if (strncmp(field, "ret", 3) == 0) {
-      if (!utils_parse_ul(value, ret, 10)) {
-        fprintf(stderr, "error: invalid return value\n");
-        return 1;
+    else if (strncmp(key, "ret", 3) == 0) {
+      if (!utils_parse_ul(val, ret, 10)) {
+        fprintf(stderr, "[unotd:server] ERROR: invalid return value: '%s'\n",
+                val);
+        return false;
       }
       got_ret = true;
     }
 
     else {
-      fprintf(stderr, "error: unknown field: \"%s\"\n", field);
-      return 1;
+      fprintf(stderr, "[unotd:server] ERROR: unknown key: '%s'\n", key);
+      return false;
     }
   }
 
   if (!got_wid) {
-    fprintf(stderr, "error: window id not set\n");
-    return 1;
+    fprintf(stderr, "[unotd:server] ERROR: missing key: 'wid'\n");
+    return false;
   }
 
   if (!got_ret) {
-    fprintf(stderr, "error: return value not set\n");
-    return 1;
+    fprintf(stderr, "[unotd:server] ERROR: missing key: 'ret'\n");
+    return false;
   }
 
-  return 0;
+  return true;
 }
 
 void protocol_handle_command(Unotd *unotd, int fd, char *buf, size_t len) {
@@ -178,22 +213,24 @@ void protocol_handle_command(Unotd *unotd, int fd, char *buf, size_t len) {
   NotificationNode *node = NULL;
 
   const char *cmd = strtok(buf, "\n");
-  if (cmd == NULL || strlen(cmd) < 3) {
-    fprintf(stderr, "error: invalid command: \"%s\"\n", cmd);
+  if (cmd == NULL) {
+    fprintf(stderr, "[unotd:server] ERROR: missing command\n");
     goto ERROR;
   }
 
-  if (strncmp(cmd, "MSG", 3) == 0) {
+  if (strncmp(cmd, "NTF", 3) == 0) {
 
     node = calloc(1, sizeof(NotificationNode));
     ASSERT(node && "protocol_handle_command: malloc failed");
     Notification *not = &node->notification;
-    not->type = UNOT_TYPE_MESSAGE;
     not->state = UNOT_NEED_INIT;
     not->timeout = unotd->config.timeout;
 
-    if (_parse_fields(unotd, &node->notification) != 0)
+    if (!_parse_ntf_fields(unotd, &node->notification)) {
+      free(not->text);
+      free(node);
       goto ERROR;
+    }
 
     pthread_mutex_lock(&unotd->nlist_lock);
 
@@ -206,34 +243,14 @@ void protocol_handle_command(Unotd *unotd, int fd, char *buf, size_t len) {
 
     pthread_mutex_unlock(&unotd->nlist_lock);
 
-    snprintf(response, 32, "OK\n\n");
-    goto OK;
-  }
+    size_t offset = 0;
+    offset += snprintf(response + offset, 32 - offset, "OK\n");
 
-  else if (strncmp(cmd, "SPN", 3) == 0) {
+    if (not->type == UNOT_TYPE_SPINNER)
+      offset +=
+          snprintf(response + offset, 32 - offset, "wid:%lu\n", not->window);
 
-    node = calloc(1, sizeof(NotificationNode));
-    ASSERT(node && "protocol_handle_command: malloc failed");
-    Notification *not = &node->notification;
-    not->type = UNOT_TYPE_SPINNER;
-    not->state = UNOT_NEED_INIT;
-    not->timeout = unotd->config.timeout;
-
-    if (_parse_fields(unotd, &node->notification) != 0)
-      goto ERROR;
-
-    pthread_mutex_lock(&unotd->nlist_lock);
-
-    nlist_append(&unotd->open, node);
-    pthread_cond_signal(&unotd->nlist_empty);
-
-    while (node->notification.window == 0) {
-      pthread_cond_wait(&unotd->notif_open, &unotd->nlist_lock);
-    }
-
-    pthread_mutex_unlock(&unotd->nlist_lock);
-
-    snprintf(response, 32, "OK\nwid:%lu\n\n", node->notification.window);
+    offset += snprintf(response + offset, 32 - offset, "\n");
     goto OK;
   }
 
@@ -241,7 +258,7 @@ void protocol_handle_command(Unotd *unotd, int fd, char *buf, size_t len) {
 
     unsigned long wid;
     unsigned long ret;
-    if (_parse_ret_fields(&wid, &ret) != 0)
+    if (!_parse_ret_fields(&wid, &ret))
       goto ERROR;
 
     pthread_mutex_lock(&unotd->nlist_lock);
@@ -259,30 +276,28 @@ void protocol_handle_command(Unotd *unotd, int fd, char *buf, size_t len) {
     else {
       target = nlist_find(&unotd->open, &prev, wid);
       if (!target) {
-        fprintf(stderr, "error: unknown window id: %lu\n", wid);
+        fprintf(stderr, "[unotd:server] ERROR: unknown notification id '%lu'\n",
+                wid);
         pthread_mutex_unlock(&unotd->nlist_lock);
         goto ERROR;
       }
       target->notification.state = UNOT_NEED_REDRAW;
     }
-
     utils_transform_notification(&target->notification, ret);
-    snprintf(response, 32, "OK\n\n");
+
     pthread_mutex_unlock(&unotd->nlist_lock);
+
+    snprintf(response, 32, "OK\n\n");
     goto OK;
   }
 
   else {
 
-    fprintf(stderr, "error: unknown command\n");
+    fprintf(stderr, "[unotd:server] ERROR: unknown command '%s'\n", cmd);
     goto ERROR;
   }
 
 ERROR:
-  if (node != NULL) {
-    free((void *)node->notification.text);
-    free(node);
-  }
   snprintf(response, 32, "ERROR\n\n");
 
 OK:
