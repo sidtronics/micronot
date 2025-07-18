a810dc821b03b451f2d620f1eafb1c7fe8beb048
Siddhesh Dharme <siddheshdharme18@gmail.com>
2025-07-18 23:14:35 +0530

    fix: fixed broken colors on CMAKE_BUILD_TYPE=Release

--- src/daemon/utils.c
+++ src/daemon/utils.c
@@ -146,8 +146,8 @@ XftColor *utils_allocate_color_s(Display *dpy, const char *color_str,
                                  XftColor *res) {
 
   unsigned long color;
-  ASSERT(utils_parse_ul(color_str, &color, 16) &&
-         "utils_allocate_color: error parsing color string");
+  bool parsed = utils_parse_ul(color_str, &color, 16);
+  ASSERT(parsed && "utils_allocate_color: error parsing color string");
 
   return utils_allocate_color(dpy, color, res);
 }
